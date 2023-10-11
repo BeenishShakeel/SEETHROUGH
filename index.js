0826234b4a57ab0676695cc0d914132fc448012a
Muhammad Ali Taimoor <alitaimoor589@gmail.com>
2023-10-11 21:03:49 +0500

    Firebase cloud messaging setup

--- index.js
+++ index.js
@@ -2,39 +2,55 @@
  * @format
  */
 
-import { AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import BackgroundService from 'react-native-background-actions';
 import RNShake from 'react-native-shake';
 import invokeApp from 'react-native-invoke-app';
+import notifee from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 
 AppRegistry.registerComponent(appName, () => App);
 
+function onMessageReceived(message) {
+	console.log("Notification data: ", message.data);
+	notifee.displayNotification({
+		title: "Incoming call",
+		body: "Somebody needs your help",
+		android: {
+			channelId: 'volunteerhelp',
+		},
+	});
+}
+
+messaging().onMessage(onMessageReceived);
+messaging().setBackgroundMessageHandler(onMessageReceived);
+
 const veryIntensiveTask = async () => {
-    await new Promise( async () => {
-        RNShake.addListener(() => {
-            invokeApp();
-        });
-    });
+	await new Promise(async () => {
+		RNShake.addListener(() => {
+			invokeApp();
+		});
+	});
 };
 
 const options = {
-    taskName: 'Example',
-    taskTitle: 'ExampleTask title',
-    taskDesc: 'ExampleTask description',
-    taskIcon: {
-        name: 'ic_launcher',
-        type: 'mipmap',
-    },
-    color: '#ff00ff',
-    parameters: {
-        delay: 1000,
-    },
+	taskName: 'Example',
+	taskTitle: 'ExampleTask title',
+	taskDesc: 'ExampleTask description',
+	taskIcon: {
+		name: 'ic_launcher',
+		type: 'mipmap',
+	},
+	color: '#ff00ff',
+	parameters: {
+		delay: 1000,
+	},
 };
 
 async function callFunctions() {
-    await BackgroundService.start(veryIntensiveTask, options);
+	await BackgroundService.start(veryIntensiveTask, options);
 }
 
 callFunctions();
--- src/Aboutus.js
+++ src/Aboutus.js
@@ -1,100 +1,130 @@
-import React, {useState ,  useEffect} from "react";
-import {View, Text, StyleSheet ,ImageBackground, Image ,ToastAndroid} from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, ImageBackground, Image, ToastAndroid, Alert } from "react-native";
 import Background from "./background";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import messaging from '@react-native-firebase/messaging';
 import Btn1 from "../assets/buttons/btn1";
 import TextField from "./textField";
 import Icon from "react-native-vector-icons/Ionicons";
 import { colors } from "../assets/constants/colors";
+import axios from "axios";
 
- //        <ImageBackground
-			// source={require('../assets/images/new4.jpg')}
-			// resizeMode='cover'
-			// style={styles.imageBackground}
-			// imageStyle={{ opacity: 0.6 }}>
+//        <ImageBackground
+// source={require('../assets/images/new4.jpg')}
+// resizeMode='cover'
+// style={styles.imageBackground}
+// imageStyle={{ opacity: 0.6 }}>
 
-export default function Aboutus({navigation}){
+export default function Aboutus({ navigation }) {
   const images = [
     require('../assets/images/12.jpg'),
-   
+
     require('../assets/images/112.jpg'),
     require('../assets/images/13.jpg'),
     require('../assets/images/15.jpg'),
     require('../assets/images/16.jpg'),
     require('../assets/images/111.jpg'),
-    
+
     // Add more image paths here
   ];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
+    const getFcmToken = async () => {
+      await messaging().registerDeviceForRemoteMessages();
+      const fcmToken = await messaging().getToken();
+      if(fcmToken){
+        console.log("FCM Token: ", fcmToken);
+        axios.post("http://192.168.18.11:8000/notify", {deviceId: fcmToken})
+        .then(res => {
+          console.log("Data: ", res.data);
+        })
+        .catch(err => console.error(err));
+      }
+      else{
+        console.log("No FCM Token");
+      }
+    }
+
+    getFcmToken();
+
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000); // Change image every 3 seconds
 
-    return () => clearInterval(interval);
+    // const unsubscribe = messaging().onMessage(async remoteMessage => {
+    //   console.log(remoteMessage);
+    //   Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    // });
+
+    return () => {
+      clearInterval(interval);
+      // unsubscribe();
+    }
   }, []);
-    return(
-     <View style = {{backgroundColor:"#D5DBDB" , flex:1}}>
-     
- <Image
- source={images[currentImageIndex]}
-				style={styles.image}></Image>
-		<Text style = {{marginTop : 20 , fontSize:25, marginLeft:60, fontFamily :"Poppins-BoldItalic" , color :'black'}}>Whats Our Moto??</Text>
-		<View style = {{ marginTop :10, marginLeft:10, marginRight:45,alignItems:'center'}}>
-        <Text style ={{fontSize:19, marginTop:13, marginLeft:10, textAlign: 'justify',fontFamily :"Poppins-Italic" , color :'black' }}>See Through My Eyes is a free app for receiving video 
-        support  Every day, sighted
-         volunteers and professionals
+
+
+  return (
+    <View style={{ backgroundColor: "#D5DBDB", flex: 1 }}>
+
+      <Image
+        source={images[currentImageIndex]}
+        style={styles.image}></Image>
+      <Text style={{ marginTop: 20, fontSize: 25, marginLeft: 60, fontFamily: "Poppins-BoldItalic", color: 'black' }}>Whats Our Moto??</Text>
+      <View style={{ marginTop: 10, marginLeft: 10, marginRight: 45, alignItems: 'center' }}>
+        <Text style={{ fontSize: 19, marginTop: 13, marginLeft: 10, textAlign: 'justify', fontFamily: "Poppins-Italic", color: 'black' }}>See Through My Eyes is a free app for receiving video
+          support  Every day, sighted
+          volunteers and professionals
           their eyes to
           solve tasks big and small to help
-           blind and low-vision  
-           people lead independent life
-           </Text>
-               </View>
-             <View style = {{marginBottom :10,marginLeft:10,flexDirection:'row'}}> 
-             <View style ={{flexDirection:'column'}}>
-             <View style = {{backgroundColor:"#1F4A83" , marginLeft:30, marginTop:80, borderRadius:50,width:50,height:50}}>
-               <MaterialIcons style = {{marginTop:8 , marginLeft:10}}name="call" size = {30} color= {'white'}></MaterialIcons>
-               </View>
-               <Text style = {{fontSize:13,marginTop:10, marginLeft:26, fontFamily :"Poppins-SemiBold" , color :'black'}}>Free calls</Text>
-             </View>
-             <View style = {{flexDirection:'column'}}>
-             <View style = {{backgroundColor:'#1F4A83' , marginLeft:65, marginTop:80, borderRadius:50,width:50,height:50}}>
-             {/* <Icon name="moon-outline" size={40} style={{ marginLeft:10 , marginTop: 5 }} color={'white'} /> */}
-             <MaterialCommunityIcons  style = {{marginTop:6, marginLeft:7}} name="hours-24" size = {35} color= {'white'}></MaterialCommunityIcons>
-               </View>
-               <Text style = {{fontSize:13, marginLeft:35, marginTop:10, fontFamily :"Poppins-SemiBold" , color :'black'}}>Available 24/7</Text>
-             </View>
-             <View style = {{flexDirection:'column'}}>
-             {/* #15317E */}
-             <View style = {{backgroundColor:'#1F4A83' , marginLeft:50, marginTop:80, borderRadius:50,width:50,height:50}}>
-             <MaterialIcons  style = {{marginTop:8 , marginLeft:10}} name="security" size = {30} color= {'white'}></MaterialIcons>
-               </View>  
-           <Text style = {{fontSize:13,  marginTop:10, marginLeft:40, fontFamily :"Poppins-SemiBold" , color :'black'}}>Fully Secure</Text>
-             </View>
-              </View>
-              
-    
-</View>
-           
-    )
+          blind and low-vision
+          people lead independent life
+        </Text>
+      </View>
+      <View style={{ marginBottom: 10, marginLeft: 10, flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'column' }}>
+          <View style={{ backgroundColor: "#1F4A83", marginLeft: 30, marginTop: 80, borderRadius: 50, width: 50, height: 50 }}>
+            <MaterialIcons style={{ marginTop: 8, marginLeft: 10 }} name="call" size={30} color={'white'}></MaterialIcons>
+          </View>
+          <Text style={{ fontSize: 13, marginTop: 10, marginLeft: 26, fontFamily: "Poppins-SemiBold", color: 'black' }}>Free calls</Text>
+        </View>
+        <View style={{ flexDirection: 'column' }}>
+          <View style={{ backgroundColor: '#1F4A83', marginLeft: 65, marginTop: 80, borderRadius: 50, width: 50, height: 50 }}>
+            {/* <Icon name="moon-outline" size={40} style={{ marginLeft:10 , marginTop: 5 }} color={'white'} /> */}
+            <MaterialCommunityIcons style={{ marginTop: 6, marginLeft: 7 }} name="hours-24" size={35} color={'white'}></MaterialCommunityIcons>
+          </View>
+          <Text style={{ fontSize: 13, marginLeft: 35, marginTop: 10, fontFamily: "Poppins-SemiBold", color: 'black' }}>Available 24/7</Text>
+        </View>
+        <View style={{ flexDirection: 'column' }}>
+          {/* #15317E */}
+          <View style={{ backgroundColor: '#1F4A83', marginLeft: 50, marginTop: 80, borderRadius: 50, width: 50, height: 50 }}>
+            <MaterialIcons style={{ marginTop: 8, marginLeft: 10 }} name="security" size={30} color={'white'}></MaterialIcons>
+          </View>
+          <Text style={{ fontSize: 13, marginTop: 10, marginLeft: 40, fontFamily: "Poppins-SemiBold", color: 'black' }}>Fully Secure</Text>
+        </View>
+      </View>
+
+
+    </View>
+
+  )
 }
 const styles = StyleSheet.create({
-    imageBackground: {
-		flex: 1,
-        width:400,
-		height:700,
-		backgroundColor: '#2A5EE0',
-	},
-	image: {
-		width: '100%',
-       
-      
-		height: 220,
-    
-		
-		
-	},
+  imageBackground: {
+    flex: 1,
+    width: 400,
+    height: 700,
+    backgroundColor: '#2A5EE0',
+  },
+  image: {
+    width: '100%',
+
+
+    height: 220,
+
+
+
+  },
 
 });
